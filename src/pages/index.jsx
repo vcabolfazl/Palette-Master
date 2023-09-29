77b82071dde6d9c64385923f870df810d1605249
vcabolfazl <104507434+vcabolfazl@users.noreply.github.com>
2023-09-29 09:37:56 +0330

    feat => development of index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
-      <center>Test Text</center>
+      <div className="zoom absolute top-0 left-0 right-0 bottom-0 w-full h-full inset-0 bg-black/40">
+        <div className="text-center">
+          <h2 className="mb-10 text-2xl md:text-5xl">Select the palette design environment</h2>
+          <div className="flex flex-wrap gap-7 items-center justify-center">
+            <Link href={"/Web"} className="px-10 py-3 bg-yellow-400 rounded-xl md:text-3xl">Web Design</Link>
+            <Link href={"/Mobile"} className="px-10 py-3 bg-rose-400 rounded-xl md:text-3xl">Mobile Design</Link>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
