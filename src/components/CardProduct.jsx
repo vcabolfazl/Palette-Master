f79e2036692e5a6814cf9c56106584173ba9a257
vcabolfazl <104507434+vcabolfazl@users.noreply.github.com>
2023-09-29 11:11:40 +0330

    feat => development of Card Product Component

---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function CardProduct({ bg, title, desc, bgBtn }) {
+
+ return (
+  <div className=" w-fit rounded-lg" style={{ background: bg }}>
+   <div className="flex p-2 gap-1">
+    <div className="">
+     <span className="bg-blue-500 inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+    <div className="circle">
+     <span className="bg-purple-500 inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+    <div className="circle">
+     <span className="bg-pink-500 box inline-block center w-3 h-3 rounded-full"></span>
+    </div>
+   </div>
+   <div className="card__content">
+    <img src="/assets/Images/web/p2.png" alt="" />
+    <div className="text-black">
+     <p className='mb-2 text-lg font-semibold' style={{ color: title }}>Lorem ipsum dolor sit</p>
+     <span className='text-left' style={{ color: desc }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ad reprehenderit amet!</span>
+     <button type="button" className="rounded-lg text-sm px-5 py-2.5 my-2 mx-auto block text-white" style={{ background: bgBtn }}>Buy </button>
+    </div>
+   </div>
+  </div>
+ )
+}
--- src/pages/Web.jsx
+++ src/pages/Web.jsx
@@ -94,6 +94,7 @@ export default function Web() {
     />
 
    </form>
+   
   </div>
  )
 }
